d41b1a2a01f3b892f37ae026f06bd481bda9b534
SergeyPrivalov31 <privalov.s.a89@gmail.com>
2021-03-22 08:58:41 +0300

    Bootstrap Form

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { Form, Container, Button } from "react-bootstrap";
 
 class Contacts extends Component {
 	render() {
 		return (
-			<div>
-				Contacts
-			</div>
+			<Container style={{ width: '500px' }}>
+				<h1 className="text-center"> Contact us </h1>
+				<Form>
+					<Form.Group controlId="formBasicEmail">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control type="email" placeholder="Enter email" />
+						<Form.Text>
+							We'll never share your email with anone else
+						</Form.Text>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicPassword">
+						<Form.Label>Example textarea</Form.Label>
+						<Form.Control as="textarea" rows="3" />
+					</Form.Group>
+
+					<Form.Group controlId="formBasicCheckbox">
+						<Form.Check type="checkbox" label="Check me out" />
+					</Form.Group>
+					<Button variant="primary" type="submit" >Submit</Button>
+
+				</Form>
+			</Container>
 		)
 	}
 }
